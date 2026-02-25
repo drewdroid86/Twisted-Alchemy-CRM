999363b67e894de3ad8df5d5cc0381aed0c23103
jdrew2517 <jdrew2517@gmail.com>
2026-02-25 04:34:47 +0000

    changes to firebase config.

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAFQvCDPG7TEdrawbyj-eylUyz44X_zGBc",
-  authDomain: "twistedalchemy-crm.firebaseapp.com",
-  projectId: "twistedalchemy-crm",
-  storageBucket: "twistedalchemy-crm.firebasestorage.app",
-  messagingSenderId: "203366267658",
-  appId: "1:203366267658:web:e173ffdea8eb8c57ff58f3",
-  measurementId: "G-6GME690775"
-};
+    authDomain: "twistedalchemy-crm.firebaseapp.com",
+      projectId: "twistedalchemy-crm",
+        storageBucket: "twistedalchemy-crm.firebasestorage.app",
+          messagingSenderId: "203366267658",
+            appId: "1:203366267658:web:e173ffdea8eb8c57ff58f3",
+              measurementId: "G-6GME690775"
+              };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { app, db, auth };
+              // Initialize Firebase
+              const app = initializeApp(firebaseConfig);
+              const analytics = getAnalytics(app);
